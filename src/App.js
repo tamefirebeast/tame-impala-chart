3a6a3856b472c06dc90ee263b817352a2d463d73
Sasha Koss <koss@nocorp.me>
2020-03-02 15:17:47 +0100

    Write and read documents

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as firebase from "firebase/app";
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  // 1. Create state to store the songs
+  const [songs, setSongs] = useState();
+  useEffect(() => {
+    // 2. Query all documents in the songs collection
+    firebase
+      .firestore()
+      .collection("songs")
+      .get()
+      // 3. When the query is resolved, save the documents
+      // to the state
+      .then(songs => setSongs(songs.docs));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1>My Favourite Tame Impala Songs</h1>
+
+      <ul>
+        {songs
+          ? songs.map(song => (
+              <li key={song.id}>
+                {song.data().title} ({song.data().rating})
+              </li>
+            ))
+          : "Loading..."}
+      </ul>
+
+      <h2>Add new song:</h2>
+
+      <form name="song" onSubmit={handleSubmit}>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <label>
+            Title: <input name="title" required />
+          </label>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <p>
+          <label>
+            Rating:{" "}
+            <input name="rating" type="number" min="0" max="100" required />
+          </label>
+        </p>
+
+        <p>
+          <button type="submit">Add to chart!</button>
+        </p>
+      </form>
     </div>
   );
 }
 
-export default App;
+async function handleSubmit(e) {
+  e.preventDefault();
+  const form = e.target;
+
+  // 1. Get data from the form
+  const title = form.title.value;
+  const rating = parseInt(form.rating.value);
+
+  // 2. Get Firestore instance
+  const firestore = firebase.firestore();
+
+  // 3. Add a document to the songs collection with random id
+  const song = await firestore.collection("songs").add({ title, rating });
+
+  alert(`Added ${title} song with id ${song.id}`);
+  form.reset();
+}
