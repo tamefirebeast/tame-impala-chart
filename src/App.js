9119a388392f2ffc8ea326fc5972979ca44adeeb
Sasha Koss <koss@nocorp.me>
2020-03-05 11:47:30 +0100

    First part of authentication

--- src/App.js
+++ src/App.js
@@ -3,6 +3,23 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 export default function App() {
+  // 1. Create a state to store the user.
+  // When the user's undefined, then the auth isn't initialized.
+  // When it's null, then the user is not logged in.
+  const [user, setUser] = useState();
+  useEffect(() => {
+    // 2. Add auth state change listener.
+    firebase.auth().onAuthStateChanged(userData => {
+      if (userData) {
+        // 3. Save the user data to the state if it's logged in.
+        setUser(userData);
+      } else {
+        // 4. Set the user to null to indicate that it's not logged in.
+        setUser(null);
+      }
+    });
+  }, []);
+
   const [songs, setSongs] = useState();
   useEffect(() => {
     return firebase
@@ -11,68 +28,85 @@ export default function App() {
       .onSnapshot(songs => setSongs(songs.docs));
   }, []);
 
-  return (
-    <div className="App">
-      <h1>My Favourite Tame Impala Songs</h1>
+  if (user === undefined) {
+    // 5. Display loading.
+    return <div className="App">Loading...</div>;
+  } else if (user === null) {
+    // 6. Display the sign in button.
+    return (
+      <div className="App">
+        <h1>Please sign in</h1>
+        <button
+          onClick={() => {
+            // TODO: Add the sign in handler.
+          }}
+        >
+          Sign in with Google
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <h1>My Favourite Tame Impala Songs</h1>
 
-      <ul>
-        {songs
-          ? songs.map(song => {
-              const data = song.data();
-              return (
-                <li key={song.id}>
-                  {song.data().title}{" "}
-                  <input
-                    value={data.rating}
-                    onChange={e =>
-                      song.ref.update({ rating: parseInt(e.target.value) })
-                    }
-                    type="number"
-                    min="0"
-                    max="100"
-                    required
-                  />{" "}
-                  <button
-                    onClick={e => {
-                      e.preventDefault();
-                      // 2. Confirm the deletion
-                      const confirmDelete = window.confirm(
-                        `Do you want to remove ${data.input}?`
-                      );
-                      // 3. Delete the component; delete returns a promise
-                      confirmDelete && song.ref.delete();
-                    }}
-                  >
-                    Remove
-                  </button>
-                </li>
-              );
-            })
-          : "Loading..."}
-      </ul>
+        <ul>
+          {songs
+            ? songs.map(song => {
+                const data = song.data();
+                return (
+                  <li key={song.id}>
+                    {song.data().title}{" "}
+                    <input
+                      value={data.rating}
+                      onChange={e =>
+                        song.ref.update({ rating: parseInt(e.target.value) })
+                      }
+                      type="number"
+                      min="0"
+                      max="100"
+                      required
+                    />{" "}
+                    <button
+                      onClick={e => {
+                        e.preventDefault();
+                        const confirmDelete = window.confirm(
+                          `Do you want to remove ${data.input}?`
+                        );
+                        confirmDelete && song.ref.delete();
+                      }}
+                    >
+                      Remove
+                    </button>
+                  </li>
+                );
+              })
+            : "Loading..."}
+        </ul>
 
-      <h2>Add new song:</h2>
+        <h2>Add new song:</h2>
 
-      <form name="song" onSubmit={handleSubmit}>
-        <p>
-          <label>
-            Title: <input name="title" required />
-          </label>
-        </p>
+        <form name="song" onSubmit={handleSubmit}>
+          <p>
+            <label>
+              Title: <input name="title" required />
+            </label>
+          </p>
 
-        <p>
-          <label>
-            Rating:{" "}
-            <input name="rating" type="number" min="0" max="100" required />
-          </label>
-        </p>
+          <p>
+            <label>
+              Rating:{" "}
+              <input name="rating" type="number" min="0" max="100" required />
+            </label>
+          </p>
 
-        <p>
-          <button type="submit">Add to chart!</button>
-        </p>
-      </form>
-    </div>
-  );
+          <p>
+            <button type="submit">Add to chart!</button>
+          </p>
+        </form>
+      </div>
+    );
+  }
 }
 
 async function handleSubmit(e) {
